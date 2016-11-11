a091fc4c59de53993e7cf5b2796c3cd9beb22467
Forrest Stone <iloveburningman@gmail.com>
2016-11-10 22:09:07 -0800

    working on the third

--- scrips/pong.js
+++ scrips/pong.js
@@ -1,28 +1,133 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-context.beginPath();
-    var paddelLeft = context.rect(10, 10, 10, 100);
-    var paddelRight = context.rect(580, 10, 10, 100);
-    //context.rect(188, 50, 200, 100);
-    context.fillStyle = 'yellow';
-    context.fill();
-    context.stroke();
+var animate = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback) { window.setTimeout(callback, 1000/60) };
 
-context.beginPath(); //midline
-    context.moveTo(300, 600);
-    context.lineTo(300, 0);
-    context.lineWidth = 1;
-    context.strokeStyle = '#ff0000';
-    context.stroke();
+var ball;
+var computer;
+var player;
 
-context.beginPath();
-    context.arc(350, 150, 50, 0, 2* Math.PI);
-    context.strokeStyle = 'red';
-    context.fillStyle = 'red';
-    context.stroke();
-    context.fill();
-    context.closePath();
+function Paddle(x, y, height, width){
+    this.x = x;
+    this.y = y;
+    this.height= height;
+    this.width = width;
+    this.speed=10;
+    
+    this.move = function(){
+        this.y+= this.speed;
+        console.log('wezzzzzz in move')
+        if (this.y<0){
+            console.log('wezzzzzz in move "if"')
+            this.y=0;
+        }
+        if(this.y>=canvas.height){
+            this.y=canvas.height;
+        }
+    }
+    
+    this.render = function() {
+       context.beginPath();
+        context.rect(x, y, 10, 100);
+        context.fillStyle = 'yellow';
+        context.fill();
+        context.stroke();
+    }
+}
+
+function Player(x, y){
+    this.paddle = new Paddle(x, y, 20, 5)
+        
+    this.render = function() {
+        this.paddle.render();
+    }
+}
+
+function Computer(x, y){
+    this.paddle = new Paddle(x, y, 20, 5)  
+        
+    this.render = function() {
+        this.paddle.render();
+    }
+}
+
+function Ball(x,y){
+    this.x = x;
+    this.y = y;
+        
+    this.render = function() {
+        context.beginPath();
+            context.arc(x, y, 50, 0, 2* Math.PI);
+            context.strokeStyle = 'red';
+            context.fillStyle = 'red';
+            context.fill();
+            context.closePath();
+    }
+}
+
+var inIt = function(){
+    ball = new Ball(350, 99);
+    computer = new Computer(10,100);
+    player = new Player(500,100);
+}
+
+function render() {
+    player.paddle.move();
+    ball.render();
+    computer.render();
+    player.render();
+    
+}
+
+var step = function(timestamp){
+    render();
+    animate(step);
+}
+
+var onKeyPress = function(event){
+    console.log("onKeyPress");
+    var keycode = event.keyCode;
+    if(keycode==38){         //38= uparrow
+        player.paddle.speed = -10;
+    }
+    if(keycode==40){         //40= down arrow
+        player.paddle.speed = 10;
+    }
+}
+
+window.onload = function(){
+    inIt();
+    animate(step);
+    window.addEventListener("keypress", onKeyPress);//keypress=event onKeyPress=function
+};
+
+//
+//context.beginPath();
+//    var paddelLeft = context.rect(10, 10, 10, 100);
+//    var paddelRight = context.rect(580, 10, 10, 100);
+//    //context.rect(188, 50, 200, 100);
+//    context.fillStyle = 'yellow';
+//    context.fill();
+//    context.stroke();
+//
+//context.beginPath(); //midline
+//    context.moveTo(300, 600);
+//    context.lineTo(300, 0);
+//    context.lineWidth = 1;
+//    context.strokeStyle = '#ff0000';
+//    context.stroke();
+//
+//context.beginPath();
+//    context.arc(350, 150, 50, 0, 2* Math.PI);
+//    context.strokeStyle = 'red';
+//    context.fillStyle = 'red';
+//    context.fill();
+//    context.closePath();
 
 
 
